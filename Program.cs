7035e8c87016a5e8305cbec5733d61512182c6e9
GodWasaProgrammer <bwnils@gmail.com>
2022-12-21 03:22:28 +0100

    1 fix range of number guessing 0,101 - done 2 dont print chances on every input loop - done 3 error message if input not valid - done 4 Additionally you could also check, if the given number   is within the possible range of the random number 5 logic flaw doesnt say if too high or too low - done

--- Program.cs
+++ Program.cs
@@ -1,63 +1,94 @@
-﻿namespace HighLowNumberGuessMathAbs
+﻿using System.Runtime.CompilerServices;
+namespace HighLowNumberGuessMathAbs
 {
     internal class Program
     {
         static void Main()
         {
             int guessAsInt;
-            int secretNumber = new Random().Next(0, 100);
             int range = 5;
             int result;
+            int MaxGuess = 101;
+            int LowestGuess = 0;
             bool isParsable;
-            int chances = 5;
-            // loop on if the tryparse is succesful or not
+            string gameLoop;
             do
             {
+                int chances;
+                int secretNumber = new Random().Next(0, 101);
+                chances = 5;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Welcome to our number guessing game. \n Make a guess between zero and hundred");
                 do
                 {
                     Console.WriteLine($"Current Chances:{chances}");
-                    Console.WriteLine("Input your guess");
-                    isParsable = Int32.TryParse(Console.ReadLine(), out guessAsInt);
-                }
-                while (isParsable == false);
-
-                chances--;
-                // calculates the difference between the guess and secret number to determine if we are in range or out of range
-                result = Math.Abs(guessAsInt - secretNumber);
-                if (result < range)
-                {
-                    Console.WriteLine($"You are {result} off!");
-                }
-
-                else
-                {
-                    if(guessAsInt > secretNumber)
+                    // loop on if the tryparse is succesful or not
+                    do
                     {
-                        Console.WriteLine("Too High!");
+                        Console.WriteLine("Input your guess");
+                        isParsable = Int32.TryParse(Console.ReadLine(), out guessAsInt);
+                        if (isParsable == false)
+                        {
+                            Console.WriteLine("Your input was not acceptable. try again");
+                        }
+
+                        if (guessAsInt > MaxGuess || guessAsInt < LowestGuess)
+                        {
+                            Console.WriteLine("You were so far off you might aswell be in Narnia... \n Guess should be between 0 and 100!");
+                            isParsable = false;
+                        }
+
+                    }
+                    while (isParsable == false);
+
+                    chances--;
+                    // calculates the difference between the guess and secret number to determine if we are in range or out of range
+                    result = Math.Abs(guessAsInt - secretNumber);
+                    if (secretNumber == guessAsInt)
+                    {
+                        Console.WriteLine("You have won ze game!");
+                        break;
                     }
 
-                    if(guessAsInt < secretNumber)
+                    else
                     {
-                        Console.WriteLine("Too low!");
+                        if (result < range && guessAsInt > secretNumber)
+                        {
+                            Console.WriteLine($"Too High! You are {result} off!");
+                        }
+
+                        if (result < range && guessAsInt < secretNumber)
+                        {
+                            Console.WriteLine($"Too Low! You are {result} off!");
+                        }
+
+                        else
+                        {
+                            if (guessAsInt > secretNumber && result > range)
+                            {
+                                Console.WriteLine("Too High!");
+                            }
+
+                            if (guessAsInt < secretNumber && result > range)
+                            {
+                                Console.WriteLine("Too low!");
+                            }
+
+                        }
+                        if (chances == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("You Lose!");
+                        }
+
                     }
+                } while (chances > 0);
 
-                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("Would you like to play again? if so, type y");
+                gameLoop = Console.ReadLine();
+            } while (gameLoop == "y");
 
-                if (secretNumber == guessAsInt)
-                {
-                    Console.WriteLine("You have won ze game!");
-                    Console.WriteLine("Game Restarting");
-                    chances = 5;
-                    secretNumber = new Random().Next(0, 100);
-                }
-
-                if (chances == 0)
-                {
-                    Console.WriteLine("You Lose!");
-                    chances = -1;
-                }
-
-            } while (chances > -1);
         }
     }
 }
